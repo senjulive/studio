80588c7374264f4037c55d30d5b8f2e16422e34d
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-06 13:03:40 +0000

    https://czbzm.wapaxo.com/filedownload/82571/pngwing-com-7-(czbzm.wapaxo.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function LoginPage() {
   return (
     <main
       className="relative flex min-h-dvh items-center justify-center bg-contain bg-no-repeat bg-center p-4 bg-black"
-      style={{ backgroundImage: "url('https://czbzm.wapaxo.com/filedownload/82581/pngwing-com-12-(czbzm.wapaxo.com).png')" }}
+      style={{ backgroundImage: "url('https://czbzm.wapaxo.com/filedownload/82571/pngwing-com-7-(czbzm.wapaxo.com).png')" }}
     >
       <div className="absolute inset-0 bg-black/50 backdrop-blur-sm" />
       <div className="relative z-10">
