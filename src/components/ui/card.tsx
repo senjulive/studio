89ea4e247316840c3733116ecdf16ee58f1f560a
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-07 18:17:11 +0000

    Change app other cards background designs, colour fading from top blueis

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-md transition-shadow duration-300 hover:shadow-lg",
+      "rounded-lg border bg-gradient-to-b from-primary/5 to-card text-card-foreground shadow-md transition-shadow duration-300 hover:shadow-lg",
       className
     )}
     {...props}
