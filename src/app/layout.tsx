6cb6e9dca749f0ec1ac14b736f8ece889f45f203
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-07 18:21:22 +0000

    Change all pages background to pinkish purple to blueish purple to while

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable}`}>
-      <body className="font-body antialiased bg-secondary flex items-center justify-center p-0 sm:p-4">
-        <div className="relative w-full max-w-sm bg-background shadow-2xl h-dvh sm:h-[calc(100dvh-2rem)] sm:max-h-[900px] sm:rounded-2xl overflow-hidden">
+      <body className="font-body antialiased bg-gradient-to-b from-purple-200 via-blue-200 to-white flex items-center justify-center p-0 sm:p-4">
+        <div className="relative w-full max-w-sm bg-transparent shadow-2xl h-dvh sm:h-[calc(100dvh-2rem)] sm:max-h-[900px] sm:rounded-2xl overflow-hidden">
             <div className="h-full w-full overflow-y-auto">
               {children}
             </div>
