c7b7821d1ad9c8e596995418250ab07d2e09d412
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-07 18:45:48 +0000

    Remove all page background, don't change anything else

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable}`}>
-      <body className="font-body antialiased bg-gradient-to-b from-purple-200 via-blue-200 to-white flex items-center justify-center p-0 sm:p-4">
+      <body className="font-body antialiased bg-background flex items-center justify-center p-0 sm:p-4">
         <div className="relative w-full max-w-sm bg-transparent shadow-2xl h-dvh sm:h-[calc(100dvh-2rem)] sm:max-h-[900px] sm:rounded-2xl overflow-hidden">
             <div className="h-full w-full overflow-y-auto">
               {children}
