387d08093581eba4f4494bda74289ba64ece6aed
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-16 16:02:00 +0000

    7

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -8,4 +8,9 @@ import type { Database } from '@/lib/database.types'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
-export const createAdminClient = () => createClient<Database>(supabaseUrl, supabaseServiceKey);
+export const createAdminClient = () => createClient<Database>(supabaseUrl, supabaseServiceKey, {
+    auth: {
+        autoRefreshToken: false,
+        persistSession: false
+    }
+});
