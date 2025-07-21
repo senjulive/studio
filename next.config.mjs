1859737ea25a2a3206124b20f9c1e9925b5da712
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-21 12:35:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'assets.coincap.io',
+        port: '',
+        pathname: '/assets/icons/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
