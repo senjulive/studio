1891e9edb13ba98de358ca61c1c510f9dc5f9f62
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-22 08:32:22 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,4 @@
 import {getRequestConfig} from 'next-intl/server';
-import type {LocalePrefix} from 'next-intl/routing';
-
-export const locales = ['en', 'es', 'fr', 'zh', 'ru'];
- 
-export const localePrefix: LocalePrefix = 'always';
  
 export default getRequestConfig(async ({locale}) => ({
   messages: (await import(`../messages/${locale}.json`)).default
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,14 @@
 import createMiddleware from 'next-intl/middleware';
-import {locales, localePrefix} from './i18n';
  
 export default createMiddleware({
-  defaultLocale: 'en',
-  locales,
-  localePrefix
+  // A list of all locales that are supported
+  locales: ['en', 'es', 'fr', 'zh', 'ru'],
+ 
+  // Used when no locale matches
+  defaultLocale: 'en'
 });
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: [
-    // Enable a redirect to a matching locale at the root
-    '/',
-
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
-    '/(en|es|fr|zh|ru)/:path*',
-
-    // Enable redirects that add a locale prefix
-    // (e.g. `/pathnames` -> `/en/pathnames`)
-    '/((?!_next|.*\\..*|api).*)'
-  ]
+  matcher: ['/', '/(en|es|fr|zh|ru)/:path*']
 };
