6e6c7d7695cb86922adeab1e572ea30186431ea1
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-22 11:57:17 +0000

    🏠 use as home icons

--- src/components/icons/nav/home-icon.tsx
+++ src/components/icons/nav/home-icon.tsx
@@ -1,7 +1,25 @@
-
-import { Home } from "lucide-react";
-import * as React from "react";
+import * as React from 'react';
 
 export const HomeIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <Home {...props} />
+  // Using a foreignObject to embed HTML (the emoji span) inside an SVG
+  <svg
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <foreignObject x="0" y="0" width="24" height="24">
+      <div
+        xmlns="http://www.w3.org/1999/xhtml"
+        style={{
+          fontSize: '20px',
+          lineHeight: '24px',
+          textAlign: 'center',
+          color: 'currentColor',
+        }}
+      >
+        🏠
+      </div>
+    </foreignObject>
+  </svg>
 );
