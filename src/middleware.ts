94807bfbb00218f3beb3f1b97fb60dae62971e2a
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-22 09:24:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,14 @@
 import createMiddleware from 'next-intl/middleware';
-import {locales, defaultLocale} from './i18n-config';
-
+ 
 export default createMiddleware({
-  locales,
-  defaultLocale,
+  // A list of all locales that are supported
+  locales: ['en', 'es', 'fr', 'zh', 'ru'],
+ 
+  // Used when no locale matches
+  defaultLocale: 'en'
 });
-
+ 
 export const config = {
   // Match only internationalized pathnames
-  matcher: [
-    // Match all pathnames except for
-    // - … if they start with `/api`, `/_next` or `/_vercel`
-    // - … the ones containing a dot (e.g. `favicon.ico`)
-    '/((?!api|_next|_vercel|.*\\..*).*)',
-    // However, match all pathnames within `/`, optionally with a locale prefix
-    '/(en|es|fr|zh|ru)/:path*',
-  ],
+  matcher: ['/', '/(en|es|fr|zh|ru)/:path*']
 };
