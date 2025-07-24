0b7f6a6b3f15403e112038c0f14c059b4efe67b8
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-24 21:14:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'coin-images.coingecko.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'assets.coincap.io',
+      },
+       {
+        protocol: 'https',
+        hostname: 'i.pravatar.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.dribbble.com',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
