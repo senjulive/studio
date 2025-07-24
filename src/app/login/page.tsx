08aac90d653be340d70af0188d9386c22afe17de
Mohamed Moosa (iNkko) <inkkox@gmail.com>
2025-07-24 14:16:41 +0000

    <script src="https://unpkg.com/@lottiefiles/dotlottie-wc@0.6.2/dist/dotl

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,22 @@ import { LoginForm } from "@/components/auth/login-form";
 export default function LoginPage() {
   return (
     <main
-      className="flex min-h-dvh items-center justify-center p-4 bg-background"
+      className="flex min-h-dvh items-center justify-center p-4 bg-background overflow-hidden"
     >
-      <LoginForm />
+      <div className="relative">
+        <div className="absolute -left-1/3 top-1/2 -translate-y-1/2 w-[300px] h-[300px] z-0 hidden md:block opacity-80">
+          <dotlottie-wc 
+              src="https://lottie.host/6f880ec6-a07a-43fb-9e71-505f775c31e0/J5KkqAUDYv.lottie" 
+              style={{width: '300px', height: '300px'}} 
+              speed="1" 
+              autoplay 
+              loop>
+          </dotlottie-wc>
+        </div>
+        <div className="relative z-10">
+          <LoginForm />
+        </div>
+      </div>
     </main>
   );
 }
